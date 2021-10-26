3d69fc966ab22de8ce8bd07b97b78d8fd18e2cc1
abouhegaziah <74408015+abouhegaziah@users.noreply.github.com>
2021-10-26 20:53:45 +0200

    3 out of 4
    Finished BreadthFirst, DepthFirst, and UniformCost.

--- code/containers/BreadthFirst.java
+++ code/containers/BreadthFirst.java
@@ -1,4 +1,32 @@
 package containers;
 
-public class BreadthFirst {
+import java.util.*;
+import modules.Node;
+import modules.State;
+
+public class BreadthFirst extends Container {
+	private Queue<Node> q = new LinkedList<>();
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return q.isEmpty();
+	}
+
+	@Override
+	public void add(List<Node> nodes) {
+		// TODO Auto-generated method stub
+		nodes.forEach((node) -> {
+            if(!(cost.containsValue(node.getState()))){
+            	cost.put(node.getState(), node.getPathCost());
+            	q.add(node);
+            }
+        });	
+	}
+
+	@Override
+	public Node poll() {
+		// TODO Auto-generated method stub
+		return q.poll();
+	}
 }
--- code/modules/Node.java
+++ code/modules/Node.java
@@ -2,10 +2,28 @@ package modules;
 
 import operators.Operator;
 
-public class Node {
+public class Node implements Comparable<Node> {
     private State state;
-    private Node parent;
+	private Node parent;
     private Operator operator;
     private int depth, pathCost;
+    
+    public int getPathCost() {
+		return pathCost;
+	}
+	public void setPathCost(int pathCost) {
+		this.pathCost = pathCost;
+	}
+	public State getState() {
+		return state;
+	}
+	public void setState(State state) {
+		this.state = state;
+	}
+	@Override
+	public int compareTo(Node node) {
+		// TODO Auto-generated method stub
+		return this.getPathCost() - node.getPathCost();
+	}
 
 }
